d726fe9b8f9f86ef7450563b20c4fea374eb1316
Flambrew <andrew.matherne24@bishopireton.org>
2022-12-14 12:10:01 -0500

    boink

--- 6_Tuning_Trouble/tuning_trouble.py
+++ 6_Tuning_Trouble/tuning_trouble.py
@@ -4,16 +4,20 @@ f = open('6_Tuning_Trouble\\input.txt', 'r')
 
 s = f.read()
 
-mark = []
+def find_marker(length):
+    mark = []
+    for i, ch in enumerate(s):
+        mark += ch
+        if len(mark) > length:
+            mark.pop(0)
+        list_lead = ''
+        for a in mark:
+            if a not in list_lead:
+                list_lead += a
+        if len(list_lead) == length:
+            return [''.join(mark), i + 1]
 
-for ch in s:
-    mark += ch
-    if len(mark) == 5:
-        mark.pop(0)
-
-    
-
-print(mark)
-
+print(find_marker(4))
+print(find_marker(14))
 
 f.close()

df4346b50f07a217ff4e79736cf13bca7ebd1e07
Flambrew <matherne.a4@gmail.com>
2023-12-07 12:55:12 -0500

    bazinga

--- 2023/1/calibration.c
+++ 2023/1/calibration.c
@@ -5,20 +5,28 @@
 
 int main()
 {
-    FILE *fptr = fopen("filename.txt", "r");
-    char *in = malloc(LEN * sizeof (char));
+    FILE *fptr = fopen("./2023/1/data", "r");
+    char *in = malloc(LEN * sizeof(char));
     fgets(in, LEN, fptr);
 
-    for (int i = 0; i < LEN && *(in + i) != '\0'; ++i) {
-        int a = 10, b;
-        while (*(in + i) != '\n') {
-            if ('0' <= *(in + i) && *(in + i) <= '9') {
+    int sum = 0;
+    for (int i = -1, a, b; ++i < LEN && in[i] != '\0'; sum += a * 10 + b) {
+        a = 10;
+        while (in[i] != '\n') {
+            printf("%c ", in[i]);
+            if ('0' <= in[i] && in[i] <= '9') {
                 if (a == 10) {
-                    
+                    a = b = (in[i] - '0');
+                } else {
+                    b = (in[i] - '0');
                 }
             }
+            i++;
         }
+        printf("%d\n", sum);
     }
+
+    printf("%d\n", sum);
 
     fclose(fptr);
     free(in);
